32f1816ff0a6a1ec2070aaa364e58cc675d90cc0
erfaw <erfawn.h@gmail.com>
2026-06-27 14:22:29 +0330

    feat: clear screen with subprocess && get number from client

---
+++ 01-check-whole-num.py
@@ -0,0 +1,10 @@
+import subprocess as sp
+
+sp.call("clear", shell=True)
+
+def is_perfect_number(n):
+    return True
+
+input_num = input('This program get a number and check is it a `perfect number` or not.\n\nPlease insert a number: ')
+
+print(input_num)
