be1778f4709bad419c50385d53962ea26b4d837c
erfaw <erfawn.h@gmail.com>
2026-06-27 15:17:56 +0330

    docs: insert example perfect numbers to docstring

--- 01-check-whole-num.py
+++ 01-check-whole-num.py
@@ -1,6 +1,8 @@
 """
 What is `Perfect Number`? 
     In number theory, a perfect number is a positive integer that is equal to the sum of its positive proper divisors, that is, divisors excluding the number itself. (Google search)
+
+    It has to return True with this example values [6, 28, 496, 8128]
 """
 
 import subprocess as sp
