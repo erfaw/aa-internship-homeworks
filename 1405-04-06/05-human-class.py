76c9074e8cb760508636c7bb7f1a4770d7041f22
erfaw <erfawn.h@gmail.com>
2026-06-27 18:53:12 +0330

    feat: declare Human() class

---
+++ 1405-04-06/05-human-class.py
@@ -0,0 +1,52 @@
+class Human:
+    def __init__(self):
+        self.name = None
+        self.family = None
+        self.father_name = None
+        self.mother_name = None
+        self.mental_situation = None
+        self.eye_color = None
+        self.gender = None
+        self.tall = None
+        self.weight = None
+        self.skin_color = None
+        self.country = None
+        self.city = None
+        self.race = None
+        self.age = None
+        self.siblings = []
+        self.degree = None
+    
+    def walk(self):
+        print('walking')
+
+    def sleep(self):
+        print('sleeping')
+    
+    def run(self):
+        print('running')
+    
+    def study(self):
+        print('studying...')
+
+    def eat(self):
+        print('eating')
+    
+    def work(self):
+        print('working')
+    
+    def play(self):
+        print('playing')
+
+    def watch(self):
+        print('watching')
+
+    def read(self):
+        print('reading')
+
+    def cook(self):
+        print('cooking')
+
+    def enjoy(self):
+        pass # 😂
+    
