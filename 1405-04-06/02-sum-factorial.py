14f5f113417c6a959208838357ce9da742f6d844
erfaw <erfawn.h@gmail.com>
2026-06-27 15:51:30 +0330

    feat: make calculate_factorial() with recursive calls

--- 1405-04-06/02-sum-factorial.py
+++ 1405-04-06/02-sum-factorial.py
@@ -2,5 +2,17 @@ import subprocess as sp
 
 sp.call('clear', shell=True)
 
-_number = input('please enter a valid number:\t ')
-print(_number)
+_number = int(input('please enter a valid number:\t '))
+
+def calculate_factorial(n):
+    """
+    Calculate factorial of recieved number. with recursive way.
+    """
+    if n >= 1:
+        return n * calculate_factorial(n-1)
+    else: 
+        return 1
+
+print(
+    calculate_factorial(_number)
+)
