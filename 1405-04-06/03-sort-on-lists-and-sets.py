1d4ae2725bb0005eb7616b5483df20e320f05506
erfaw <erfawn.h@gmail.com>
2026-06-27 18:37:10 +0330

    feat: implement bubble sort manually

--- 1405-04-06/03-sort-on-lists-and-sets.py
+++ 1405-04-06/03-sort-on-lists-and-sets.py
@@ -1,11 +1,13 @@
-# def sort(iterable):
-#     return iterable.sort()
+def bubble_sort(iter):
+    for i in range(len(iter)-1):
+        for j in range(len(iter)-1-i):
+            if iter[j] > iter[j+1]:
+                temp = iter[j]
+                iter[j] = iter[j+1]
+                iter[j+1] = temp
+        
+    return iter
 
-# _list = [2, 7, 8, 1, 0, -5]
-# _set = {2, 7, 8, 1, 0, -5}
-
-# sorted_list = sort(_list)
-# print(sorted_list)
-
-# sorted_set = sort(_set)
-# print(sorted_set)
+sample_list = [35, 73, 10, 11, 2]
+sample_list = [64, 34, 25, 12, 22, 11, 90]
+print(bubble_sort(sample_list))
