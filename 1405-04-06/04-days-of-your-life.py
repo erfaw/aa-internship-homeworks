56b678727f94444744e181f223f60448481aa34f
erfaw <erfawn.h@gmail.com>
2026-06-27 17:00:17 +0330

    feat: get year and month and date separately from client and simply print it.

--- 1405-04-06/04-days-of-your-life.py
+++ 1405-04-06/04-days-of-your-life.py
@@ -3,6 +3,13 @@ import subprocess as sp
 sp.call("clear", shell=True)
 
 print(
-    "This program calculate counts of days you lived till now."
+    "This program calculate counts of days you lived till now.\nPlease insert your birthday... (in Gregorian Calendar)"
 )
 
+year = input("Year: ")
+month = input("Month: ")
+date = input("Day: ")
+
+print(year)
+print(month)
+print(date)
