5ae7d314dc7e833fb083d7d7389ba0b924ef7002
erfaw <erfawn.h@gmail.com>
2026-06-27 14:24:12 +0330

    docs: add docstring to is_perfect_number() func

--- 01-check-whole-num.py
+++ 01-check-whole-num.py
@@ -2,7 +2,10 @@ import subprocess as sp
 
 sp.call("clear", shell=True)
 
-def is_perfect_number(n):
+def is_perfect_number(n) -> bool:
+    """
+    checks a number is perfect or not. 
+    """
     return True
 
 input_num = input('This program get a number and check is it a `perfect number` or not.\n\nPlease insert a number: ')
