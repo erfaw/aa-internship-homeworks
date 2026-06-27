e78f09e35f5eb225805064a4adde8b4ea487b753
erfaw <erfawn.h@gmail.com>
2026-06-27 16:51:06 +0330

    chore: make all codes commented.

---
+++ 1405-04-06/03-sort-on-lists-and-sets.py
@@ -0,0 +1,11 @@
+# def sort(iterable):
+#     return iterable.sort()
+
+# _list = [2, 7, 8, 1, 0, -5]
+# _set = {2, 7, 8, 1, 0, -5}
+
+# sorted_list = sort(_list)
+# print(sorted_list)
+
+# sorted_set = sort(_set)
+# print(sorted_set)
