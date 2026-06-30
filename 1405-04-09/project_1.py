32c943aebd7f299e7ba33a72bf2d3fab8110b46a
erfaw <erfawn.h@gmail.com>
2026-06-30 16:40:28 +0330

    feat: get details about given username and print all

---
+++ 1405-04-09/project_1.py
@@ -0,0 +1,12 @@
+import requests as rq
+
+GITHUB_LINK = r"https://api.github.com/users"
+
+username = input('Insert a GitHub username please:\n\t>> ')
+
+with rq.get(f"{GITHUB_LINK}/{username}", timeout=5) as response:
+    data = response.json()
+
+print(data)
+
+
