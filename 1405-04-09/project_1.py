ab624b79600040859d1ee056d94081d26da303a3
erfaw <erfawn.h@gmail.com>
2026-06-30 17:06:50 +0330

    feat: catch the exception when 404 raise and user not found

--- 1405-04-09/project_1.py
+++ 1405-04-09/project_1.py
@@ -1,15 +1,18 @@
 import requests as rq
+from requests.exceptions import HTTPError
 
 GITHUB_LINK = r"https://api.github.com/users"
 
 
 username = input("Insert a GitHub username please:\n\t>> ")
-
-with rq.get(f"{GITHUB_LINK}/{username}", timeout=5) as response:
-    response.raise_for_status()
-    data = response.json()
-
-print(f"""
+try:
+    with rq.get(f"{GITHUB_LINK}/{username}", timeout=5) as response:
+        response.raise_for_status()
+        data = response.json()
+except HTTPError as e:
+    print(e)
+else:
+    print(f"""
 ====RESULT====
 Login : {data["login"]}
 ---
@@ -25,4 +28,4 @@ Public Repositories: {data["public_repos"]} | {data["repos_url"]}
 ---
 Created At : {data["created_at"]}
 ==============
-""")
+    """)
