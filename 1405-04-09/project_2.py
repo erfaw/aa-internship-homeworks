482f69e32d8888ec6751ceee78c2a829f4b02171
erfaw <erfawn.h@gmail.com>
2026-06-30 18:21:02 +0330

    refactor: move all exception catchs out of with statement, feat: catch ConnectionError with just printing message

--- 1405-04-09/project_2.py
+++ 1405-04-09/project_2.py
@@ -1,5 +1,5 @@
 import requests as rq
-from requests.exceptions import Timeout
+from requests.exceptions import Timeout, ConnectionError
 
 url = input(
     "Insert a site address (URL)'\n\t >> "
@@ -9,14 +9,12 @@ if not 'https://' in url and not 'http://' in url:
 
 try:
     with rq.get(url=url, timeout=5) as response:
-        try:
-            response.raise_for_status()
-        except ConnectionError as e:
-            print(f"Seems there is a problem with your network connection.\n{e}")
+        response.raise_for_status()
+        if response.status_code == 200:
+            print(f"Website is Online!✅ (status code: {response.status_code})")
         else:
-            if response.status_code == 200:
-                print(f"Website is Online!✅ (status code: {response.status_code})")
-            else:
-                print(f"Website is Offline!❌ (status code: {response.status_code})")
+            print(f"Website is Offline!❌ (status code: {response.status_code})")
 except Timeout: 
     print(f"Request timeout. please try again.")
+except ConnectionError as e:
+    print(e)
