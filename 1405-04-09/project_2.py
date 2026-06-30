fb355d53d1df07b39cccb54d2d469638c410dc77
erfaw <erfawn.h@gmail.com>
2026-06-30 17:44:25 +0330

    feat: catch the ConnectionError exception
    -move codes to else statement

--- 1405-04-09/project_2.py
+++ 1405-04-09/project_2.py
@@ -5,8 +5,12 @@ url = input(
 )
 
 with rq.get(url=url, timeout=10) as response:
-    response.raise_for_status()
-    if response.status_code == 200:
-        print(f"Website is Online!✅ (status code: {response.status_code})")
+    try:
+        response.raise_for_status()
+    except ConnectionError as e:
+        print(f"Seems there is a problem with your network connection.\n{e}")
     else:
-        print(f"Website is Offline!❌ (status code: {response.status_code})")
+        if response.status_code == 200:
+            print(f"Website is Online!✅ (status code: {response.status_code})")
+        else:
+            print(f"Website is Offline!❌ (status code: {response.status_code})")
