8648ba876da87443d527888539f582f15dc779a4
erfaw <erfawn.h@gmail.com>
2026-06-30 16:58:29 +0330

    feat: print a formatted result

--- 1405-04-09/project_1.py
+++ 1405-04-09/project_1.py
@@ -2,9 +2,26 @@ import requests as rq
 
 GITHUB_LINK = r"https://api.github.com/users"
 
+
 username = input("Insert a GitHub username please:\n\t>> ")
 
 with rq.get(f"{GITHUB_LINK}/{username}", timeout=5) as response:
     data = response.json()
 
-print(data)
+print(f"""
+====RESULT====
+Login : {data["login"]}
+---
+Name : {data["name"]}
+---
+Bio : {data["bio"]}
+---
+Followers : {data["followers"]}
+---
+Following : {data["following"]}
+---
+Public Repositories: {data["public_repos"]} | {data["repos_url"]} 
+---
+Created At : {data["created_at"]}
+==============
+""")
