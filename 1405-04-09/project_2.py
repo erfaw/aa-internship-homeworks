1219fed84b7a4c7d3be22fe2284212e99c311ba9
erfaw <erfawn.h@gmail.com>
2026-06-30 17:33:35 +0330

    feat: check status_code and print about it

--- 1405-04-09/project_2.py
+++ 1405-04-09/project_2.py
@@ -1,3 +1,12 @@
-url = input("Insert a site address (URL)\nURL format must be 'https://<site address>.<domain>'")
+import requests as rq
 
-print(url)
+url = input(
+    "Insert a site address (URL)\nURL format must be 'https://<site address>'"
+)
+
+with rq.get(url=url, timeout=10) as response:
+    response.raise_for_status()
+    if response.status_code == 200:
+        print("Website is Online!✅")
+    else:
+        print("Website is Offline!❌")
