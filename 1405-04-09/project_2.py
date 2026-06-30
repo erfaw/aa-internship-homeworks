d04f58964aff5630882763ba386d083e3867529d
erfaw <erfawn.h@gmail.com>
2026-06-30 18:02:13 +0330

    feat: catch exception Timeout

--- 1405-04-09/project_2.py
+++ 1405-04-09/project_2.py
@@ -1,16 +1,20 @@
 import requests as rq
+from requests.exceptions import Timeout
 
 url = input(
     "Insert a site address (URL)\nURL format must be 'https://<site address>'\n\t >> "
 )
 
-with rq.get(url=url, timeout=5) as response:
-    try:
-        response.raise_for_status()
-    except ConnectionError as e:
-        print(f"Seems there is a problem with your network connection.\n{e}")
-    else:
-        if response.status_code == 200:
-            print(f"Website is Online!✅ (status code: {response.status_code})")
+try:
+    with rq.get(url=url, timeout=5) as response:
+        try:
+            response.raise_for_status()
+        except ConnectionError as e:
+            print(f"Seems there is a problem with your network connection.\n{e}")
         else:
-            print(f"Website is Offline!❌ (status code: {response.status_code})")
+            if response.status_code == 200:
+                print(f"Website is Online!✅ (status code: {response.status_code})")
+            else:
+                print(f"Website is Offline!❌ (status code: {response.status_code})")
+except Timeout as e: 
+    print(f"Request timeout. please try again.\n\t\tError message: {e}")
